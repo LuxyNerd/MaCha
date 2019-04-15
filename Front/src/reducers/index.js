b6a2964092f670461812682551fa444c8f7d4f22
emine.turcan <emine.turcan@cgi.com>
2019-04-15 22:14:58 +0200

    added REdux store and handle with errors

--- Front/src/reducers/index.js
+++ Front/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-//import errorsReducer from './errorReducer';
+import errorsReducer from './errorReducer';
 
 export default combineReducers({
-  //errors: errorsReducer
+  errors: errorsReducer
 });
