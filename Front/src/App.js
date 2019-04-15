fca013885ea177ce435b1115c65c76dc287ec199
emine.turcan <emine.turcan@cgi.com>
2019-04-16 01:27:45 +0200

    updated full login From with MaterialUi

--- Front/src/App.js
+++ Front/src/App.js
@@ -8,19 +8,36 @@ import AddPT from './components/ProjectTask/AddPT';
 import { Provider } from 'react-redux';
 import store from './store';
 import Form from './Form';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import LoginScreen from './Loginscreen';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginPage: [],
+      uploadScreen: []
+    };
+  }
+  componentWillMount() {
+    var loginPage = [];
+    loginPage.push(<LoginScreen appContext={this} />);
+    this.setState({
+      loginPage: loginPage
+    });
+  }
   render() {
     return (
       <Provider store={store}>
         <Router>
           <div className="App">
-            <Navbar />
+            {this.state.loginPage}
+            {this.state.uploadScreen}
             {/* just show the called compontent with route*/}
             {/* <Route exact path="/projectBoard" component={ProjectBoard} /> */}
             {/* Form login*/}
-            <Route exact path="/addPT" component={AddPT} />
-            <Route exact path="/" component={Form} />
+            {/* <Route exact path="/addPT" component={AddPT} />
+            <Route exact path="/" component={Form} /> */}
           </div>
         </Router>
       </Provider>
