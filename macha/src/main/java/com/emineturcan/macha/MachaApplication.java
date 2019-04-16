c19e7b45c3f7c35d066057929ea7928997e8e426
emine.turcan <emine.turcan@cgi.com>
2019-04-16 21:44:37 +0200

    trouble

---
+++ macha/src/main/java/com/emineturcan/macha/MachaApplication.java
@@ -0,0 +1,13 @@
+package com.emineturcan.macha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachaApplication.class, args);
+    }
+
+}
