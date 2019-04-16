8126c97f9fa89cfec0709d669483ab8ea63cbad8
emine.turcan <emine.turcan@cgi.com>
2019-04-16 12:38:14 +0200

    there is hard Errors to fix

---
+++ Back/src/main/java/com/emineturcan/macha/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.emineturcan.macha.repositories;
+
+import com.emineturcan.macha.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+
+    User findByUsername(String username);
+    User getById(Long id);
+}
